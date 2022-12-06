d129511d17fc0f379043e1a10752be29d3cfff03
Cadyn Reger <116835253+creger01@users.noreply.github.com>
2022-12-06 13:36:08 -0500

    Add files via upload

---
+++ Q31.py
@@ -0,0 +1,11 @@
+#The authors' namnes are: Cadyn Reger
+def function(word):
+    total = 0
+    for x in word:
+        if x == "e":
+            total = total + 1
+    print(total)
+
+
+
+function("unforseeable")
---
+++ Q32.py
@@ -0,0 +1,5 @@
+for x in ["lamb", "ham", "chicken", "shrimp"]:
+    if x == "beef":
+        print("There is beef in the list")
+    else:
+        print("There is no beef in the list")
